0b6c9367bf38ad63993f41fdd5887dbfb7348a8a
Kurt King <kurtking@dhcp-10-105-216-201.wireless.northwestern.private>
2019-03-02 14:30:40 -0600

    message

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;     
 
 
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoheadline";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI);   
 
 // Initialize Express
 var app = express();    
